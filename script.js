0fc41717ad907de51aa91041bdc32061853bc037
Filza Ahmed <70163799+nfahmed11@users.noreply.github.com>
2022-09-06 11:02:54 -0400

    Add files via upload
    Sept 6 Changes - New Work

--- script.js
+++ script.js
@@ -24718,6 +24718,80 @@ if (
     (window.Snow = new F()), window.Snow.init();
   })();
 
+//
+
+var TxtRotate = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("txt-rotate");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-rotate");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+
+//   title section header word change
+(function () {
+  let words = ["UI/UX Designer", "Coding Coach", "Front-End Web Developer"],
+    i = 0;
+  setInterval(function () {
+    $("#changingtitle").fadeOut(function () {
+      $(this)
+        .html(words[(i = (i + 1) % words.length)])
+        .fadeIn();
+    });
+  }, 2000);
+})();
+
 //   contact section header word change
 (function () {
   let words = ["Speak", "Chit", "Chat", "Talk", "Connect"],
